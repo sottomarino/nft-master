f86d6a24720c4be1515016bef4a3a52108dafd87
sottomarino <simone.ambrosi@live.com>
2021-10-27 20:22:09 +0200

    Updates

---
+++ precache-manifest.35ede3d93ab837fc2746a20d4f842ef4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "88b67aa3ae753166ac4a",
+    "url": "/nft-master/static/css/main.1e37f0ad.chunk.css"
+  },
+  {
+    "revision": "88b67aa3ae753166ac4a",
+    "url": "/nft-master/static/js/main.88b67aa3.chunk.js"
+  },
+  {
+    "revision": "fedbd7fbc27f1e1aa58d",
+    "url": "/nft-master/static/css/1.9765a299.chunk.css"
+  },
+  {
+    "revision": "fedbd7fbc27f1e1aa58d",
+    "url": "/nft-master/static/js/1.fedbd7fb.chunk.js"
+  },
+  {
+    "revision": "d7210db24e31d3baed53",
+    "url": "/nft-master/static/js/runtime~main.d7210db2.js"
+  },
+  {
+    "revision": "c804f9471a5c658137174a784bc16797",
+    "url": "/nft-master/index.html"
+  }
+];
